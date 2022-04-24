62118e76465cf8c688bc167187bbb4c1b8300c41
osok-dev <103960156+osok-dev@users.noreply.github.com>
2022-04-24 21:45:27 +0100

    Beggining wire up of contract

---
+++ src/contracts/index.ts
@@ -0,0 +1,1 @@
+export const vaultFactoryContractAddress = "<FILL ADDRESS HERE>";
--- src/hooks/useEscrowExists.ts
+++ src/hooks/useEscrowExists.ts
@@ -1,5 +1,17 @@
-export function useEscrowExists(): boolean {
-  // TODO: implement
+import exampleContractAbi from "../abi/ExampleContract.json";
+import { ethers } from "ethers";
+import { vaultFactoryContractAddress } from "../contracts";
+import { useContractCall } from "@usedapp/core";
 
-  return false;
+const simpleContractInterface = new ethers.utils.Interface(exampleContractAbi);
+
+export function useEscrowExists(): boolean {
+  const [hasVault]: any =
+    useContractCall({
+      abi: simpleContractInterface,
+      address: vaultFactoryContractAddress,
+      method: "hasVault",
+      args: [],
+    }) ?? [];
+  return hasVault;
 }
