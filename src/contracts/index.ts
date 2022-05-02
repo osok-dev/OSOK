97e11d734e06fb9d5f6ff427b20e6feafa4b652c
cryptopop98 <104268306+cryptopop98@users.noreply.github.com>
2022-05-02 13:29:41 +0100

    New vaultFactory version (#10)
    * Update index.ts
    
    Switched to new factory address.
    
    * updated ABIs
    
    updated

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -1,4 +1,4 @@
-const TESTNET_ADDRESS = "0x3db847E4D3B6117e8ed060C8e45e2e680d5E2AAD";
+const TESTNET_ADDRESS = "0x001F3228d29330B9556AF3d3e71ab4861F15E763";
 const MAINNET_ADDRESS = "0x3db847E4D3B6117e8ed060C8e45e2e680d5E2AAD"; // TODO: change to mainnet
 
 // Possibly a more robust way to go about this
